746fbfb911010bac5f906d6d3bc194a75db31d7b
sumchive <haon0319@naver.com>
2025-12-08 09:50:04 +0900

    [setting] 프로젝트 초기 세팅

---
+++ baemin-swiftui-kimsoomin/baemin-swiftui-kimsoomin/baemin_swiftui_kimsoominApp.swift
@@ -0,0 +1,17 @@
+//
+//  baemin_swiftui_kimsoominApp.swift
+//  baemin-swiftui-kimsoomin
+//
+//  Created by mandoo on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct baemin_swiftui_kimsoominApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
