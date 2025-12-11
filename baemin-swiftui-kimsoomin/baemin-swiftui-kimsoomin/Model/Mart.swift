914754c49cfa4822f9cf57d00da06737ed3f8fe0
sumchive <haon0319@naver.com>
2025-12-11 17:31:58 +0900

    [feat] MartView 구현

---
+++ baemin-swiftui-kimsoomin/baemin-swiftui-kimsoomin/Model/Mart.swift
@@ -0,0 +1,24 @@
+//
+//  MartCategory.swift
+//  baemin-swiftui-kimsoomin
+//
+//  Created by mandoo on 12/11/25.
+//
+
+import Foundation
+
+struct Mart: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageName: String
+    
+    static let mart: [Mart] = [
+        Mart(name: "B마트", imageName: "mart1"),
+        Mart(name: "CU", imageName: "mart3"),
+        Mart(name: "이마트슈퍼", imageName: "mart2"),
+        Mart(name: "홈플러스", imageName: "mart5"),
+        Mart(name: "GS25", imageName: "mart6"),
+        Mart(name: "세븐일레븐", imageName: "mart4"),
+    ]
+}
+
---
+++ baemin-swiftui-kimsoomin/baemin-swiftui-kimsoomin/Presentation/MartView.swift
@@ -0,0 +1,34 @@
+//
+//  MartView.swift
+//  baemin-swiftui-kimsoomin
+//
+//  Created by mandoo on 12/9/25.
+//
+
+import SwiftUI
+
+struct MartView: View {
+    private let mart = Mart.mart
+    let columns = Array(repeating: GridItem(.flexible()), count: 6)
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: [GridItem(.flexible())], spacing: 20) {
+                ForEach(mart, id: \.id) {mart in
+                    VStack {
+                        Image(mart.imageName)
+                            .resizable()
+                            .frame(width: 58, height: 58)
+                            .cornerRadius(20)
+                        
+                        Text(mart.name)
+                            .font(.body_r_14)
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+        .padding(.vertical, 16)
+        .background(.baeminWhite)
+    }
+}
