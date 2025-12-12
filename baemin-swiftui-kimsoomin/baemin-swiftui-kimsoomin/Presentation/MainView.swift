f62c724a695f8727275cf48f13ac834b78baec5e
sumchive <haon0319@naver.com>
2025-12-12 13:15:08 +0900

    [feat] MainView 구현

---
+++ baemin-swiftui-kimsoomin/baemin-swiftui-kimsoomin/Presentation/MainView.swift
@@ -0,0 +1,41 @@
+//
+//  MainView.swift
+//  baemin-swiftui-kimsoomin
+//
+//  Created by mandoo on 12/9/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack(spacing: 0) {
+                HeaderView()
+                
+                VStack(spacing: 10) {
+                    FoodCategoryView()
+                    
+                    MartView()
+                    
+                    BannerView()
+                    
+                    PopularRankView()
+                    
+                    RecentOrderView()
+                    
+                    DiscountRestaurantView()
+                }
+            }
+        }
+        .modifier(ScrollClipModifier())
+        .background(.baeminBackgroundWhite)
+    }
+}
+
+struct ScrollClipModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .clipShape(Rectangle())
+  }
+}
